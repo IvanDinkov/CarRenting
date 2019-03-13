466bbc5e33849896662ae2c50819656112423747
HristiqnIliev <cessar123@abv.bg>
2019-03-13 17:32:43 +0200

    Added Form.

---
+++ Form/Data/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public static class Connection
+    {
+        public const string CONNECTION_STRING = "Server=DESKTOP-BDTJV1C\\SQLEXPRESS;Database=CarRenting;Integrated Security=true";
+    }
+}
